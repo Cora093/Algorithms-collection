ac6f65ca5ba6dbc7f8eb69748693d75e827ba485
Cora <93145731+Cora093@users.noreply.github.com>
2024-01-23 15:49:27 +0800

    init

---
+++ src/dp11.java
@@ -0,0 +1,11 @@
+/**
+ * 二维数组解01背包
+ */
+public class dp11 {
+    public static void main(String[] args) {
+        int[] weight = {1, 3, 4};
+        int[] value = {15, 20, 30};
+        int bagSize = 4;
+    }
+
+}

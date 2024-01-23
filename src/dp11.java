99daa145d1033e2339fc99b6b3986fcb6f3cb54c
Cora <93145731+Cora093@users.noreply.github.com>
2024-01-23 16:18:03 +0800

    add dp11

--- src/dp11.java
+++ src/dp11.java
@@ -6,6 +6,29 @@ public class dp11 {
         int[] weight = {1, 3, 4};
         int[] value = {15, 20, 30};
         int bagSize = 4;
+
+        // 初始化数组
+        int[][] dp = new int[weight.length + 1][bagSize + 1];
+
+        // 先遍历物品 后遍历容量
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (j < weight[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // 递推公式填充数组
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i - 1]] + value[i - 1]);
+                }
+            }
+        }
+
+        // 打印dp数组
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 
 }

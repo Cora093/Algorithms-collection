1f731b56629e4605ffa75b82498bdf060469cbf2
汤思远 <cora_0903@outlook.com>
2024-03-13 12:19:33 +0800

    add offer05

---
+++ src/offer/offer05.java
@@ -0,0 +1,34 @@
+package offer;
+
+/**
+ * 替换空格
+ * 把传入字符数组中的' '换成'&20',且传入数组保证有足够空间容纳修改后的字符
+ */
+public class offer05 {
+    public static void main(String[] args) {
+        String data = "We are happy.";
+        System.out.println(data);
+        System.out.println(replaceBlank(data));
+    }
+
+    // 双指针从后向前 时间复杂度n
+    public static char[] replaceBlank(String data) {
+        int newLength = data.replaceAll(" ", "%20").length();
+        char[] charArray = data.toCharArray();
+        char[] newArray = new char[newLength];
+        int i = charArray.length - 1;
+        int j = newArray.length - 1;
+        while (i >= 0) {
+            if (charArray[i] != ' ') {
+                newArray[j--] = charArray[i--];
+            } else {
+                newArray[j--] = '0';
+                newArray[j--] = '2';
+                newArray[j--] = '%';
+                i--;
+            }
+        }
+        return newArray;
+    }
+
+}
